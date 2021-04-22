f4e4030474921af7e0cf2789f346bff0896e029b
Pawel Klimczyk <pawel@klimczyk.pl>
2021-04-22 13:14:43 +0200

    Added JsonResultFromString class

---
+++ src/Gmtl.HandyLib.AspNetCore/Gmtl.HandyLib.AspNetCore/JsonResultFromString.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace Gmtl.HandyLib.AspNetCore
+{
+    public class JsonResultFromString : ObjectResult
+    {
+        private static MediaTypeCollection contentTypes = new MediaTypeCollection { System.Net.Mime.MediaTypeNames.Application.Json };
+
+        public JsonResultFromString(object value) : base(value)
+        {
+            StatusCode = 200;
+            ContentTypes = contentTypes;
+        }
+    }
+}
