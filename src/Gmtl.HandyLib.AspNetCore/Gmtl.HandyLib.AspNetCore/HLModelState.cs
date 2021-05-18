005058ced83773bb7ab956490ea61204a358dd32
Pawel Klimczyk <pawel@klimczyk.pl>
2021-05-18 13:40:22 +0200

    Added HLModelState

---
+++ src/Gmtl.HandyLib.AspNetCore/Gmtl.HandyLib.AspNetCore/HLModelState.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Gmtl.HandyLib.AspNetCore
+{
+    public static class HLModelState
+    {
+        public static Dictionary<string, string> GetErrors(this ModelStateDictionary modelState)
+        {
+            Dictionary<string, string> errors = new Dictionary<string, string>();
+
+            if (modelState.IsValid)
+            {
+                return errors;
+            }
+
+            foreach (var item in modelState)
+            {
+                foreach (var error in item.Value.Errors)
+                {
+                    if (!errors.ContainsKey(item.Key))
+                    {
+                        //errors.Add(item.Key, error.ErrorMessage);
+                    }
+                }
+
+                string errorsForKey = item.Value.Errors
+                    .Select(e => e.ErrorMessage)
+                    .Aggregate("", //handle empty list case.
+                        (current, next) => current + ". " + next);
+
+                errors.Add(item.Key, errorsForKey);
+            }
+
+            return errors;
+        }
+    }
+}
