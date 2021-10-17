0ac4b39ba040198d73f160f28780f32aadfd8fcf
Pawel Klimczyk <pawel@klimczyk.pl>
2021-10-17 19:46:23 +0200

    Added HLHttpRequest

---
+++ src/Gmtl.HandyLib.AspNetCore/Gmtl.HandyLib.AspNetCore/HLHttpRequest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Gmtl.HandyLib.AspNetCore
+{
+    public static class HLHttpRequest
+    {
+        /// <summary>
+        /// Check if request contains expected key
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="key">case insensitive key</param>
+        /// <returns></returns>
+        public static bool HasQueryString(this HttpRequest request, string key)
+        {
+            return request.Query.ContainsKey(key);
+        }
+
+        /// <summary>
+        /// Returns value for querystring key or String.Empty
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="key">case insensitive key</param>
+        /// <returns></returns>
+        public static string HasQueryStringValue(this HttpRequest request, string key)
+        {
+            return HasQueryString(request, key) ? request.Query[key] : string.Empty;
+        }
+    }
+}
