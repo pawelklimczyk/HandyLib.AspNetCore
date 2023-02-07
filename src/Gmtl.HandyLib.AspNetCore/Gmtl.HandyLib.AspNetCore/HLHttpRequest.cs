eb329a43bcd315fc6f53e80047ad64ae6e9b769f
Pawel Klimczyk <pawel@klimczyk.pl>
2023-02-07 12:54:35 +0100

    Added HttpRequest.GetRawBodyAsync

--- src/Gmtl.HandyLib.AspNetCore/Gmtl.HandyLib.AspNetCore/HLHttpRequest.cs
+++ src/Gmtl.HandyLib.AspNetCore/Gmtl.HandyLib.AspNetCore/HLHttpRequest.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Gmtl.HandyLib.AspNetCore
 {
@@ -24,6 +27,27 @@ namespace Gmtl.HandyLib.AspNetCore
         public static string HasQueryStringValue(this HttpRequest request, string key)
         {
             return HasQueryString(request, key) ? request.Query[key] : string.Empty;
+        }
+
+        public static async Task<string> GetRawBodyAsync(this HttpRequest request, Encoding encoding = null)
+        {
+            if (!request.Body.CanSeek)
+            {
+                // We only do this if the stream isn't *already* seekable,
+                // as EnableBuffering will create a new stream instance
+                // each time it's called
+                request.EnableBuffering();
+            }
+
+            request.Body.Position = 0;
+
+            var reader = new StreamReader(request.Body, encoding ?? Encoding.UTF8);
+
+            var body = await reader.ReadToEndAsync().ConfigureAwait(false);
+
+            request.Body.Position = 0;
+
+            return body;
         }
     }
 }
