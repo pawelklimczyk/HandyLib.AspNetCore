d3b4c06fd8ac778a57d0d2436b3b713e64451ab3
Pawel Klimczyk <pawel@klimczyk.pl>
2023-08-13 19:34:48 +0200

    Added StringTrimmerModelBinder

---
+++ src/Gmtl.HandyLib.AspNetCore/Gmtl.HandyLib.AspNetCore/ModelBinders/StringTrimmerModelBinderProvider.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Threading.Tasks;
+
+namespace Gmtl.HandyLib.AspNetCore.ModelBinders;
+
+/// <summary>
+/// Usage
+//services.AddControllers(opt =>
+///{
+///    //registers CustomModelBinderProvider in the first place so that it is asked before other model binder providers.
+///    opt.ModelBinderProviders.Insert(0, new StringTrimmerModelBinderProvider());
+///});
+/// </summary>
+public class StringTrimmerModelBinderProvider : IModelBinderProvider
+{
+    public IModelBinder GetBinder(ModelBinderProviderContext context)
+    {
+        if (context is null)
+            throw new ArgumentNullException(nameof(context));
+
+
+        if (context.Metadata.ModelType == typeof(string) && context.BindingInfo.BindingSource != BindingSource.Body)
+            return new StringTrimmerBinder();
+
+        return null;
+    }
+
+    public class StringTrimmerBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext is null)
+                throw new ArgumentNullException(nameof(bindingContext));
+
+            var modelName = bindingContext.ModelName;
+            var valueProviderResult = bindingContext.ValueProvider.GetValue(modelName);
+
+            if (valueProviderResult == ValueProviderResult.None)
+                return Task.CompletedTask;
+
+            bindingContext.Result = ModelBindingResult.Success(valueProviderResult.FirstValue.Trim());
+            return Task.CompletedTask;
+        }
+    }
+}
+
